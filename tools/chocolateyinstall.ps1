69d7a262dabbb783d35bb843d942db571fc62831
Yad <yad.maintainers@gmail.com>
2017-06-02 01:34:41 -0700

    Release 20161025

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= 'msys2-installer'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'http://repo.msys2.org/distrib/i686/msys2-i686-20160921.exe'
-$url64 = 'http://repo.msys2.org/distrib/x86_64/msys2-x86_64-20160921.exe'
+$url = 'http://repo.msys2.org/distrib/i686/msys2-i686-20161025.exe'
+$url64 = 'http://repo.msys2.org/distrib/x86_64/msys2-x86_64-20161025.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -13,9 +13,9 @@ $packageArgs = @{
   url64bit      = $url64
   
   # Checksums can be found here: http://www.msys2.org/
-  checksum      = '5bbcec0925d6e0a0b870287fbe2de87e42be968c4e17937848b52402df1d1f28'
+  checksum      = '4951a47177777a54c7ad4ac99755ba4bbdf1a0cb23a174a72d91f71dc25bcb15'
   checksumType  = 'sha256'
-  checksum64    = '5630197faf072b382bb1b0147b2fd2462f0fad49af42436eeb7bbd4e31b28d67'
+  checksum64    = '2c198787ea1c4be39ff80466c4d831f8c7f06bd56d6d190bf63ede35292e344c'
   checksumType64= 'sha256'
 
   # WARNING: Installer seems to get stuck on "SHOW FINISHED PAGE" page when you use -v (verbose)
